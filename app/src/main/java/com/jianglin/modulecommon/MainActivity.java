ae56611c2d301804f7d4fa8a2bf58dfa1fc61952
jiang <jianglinair@163.com>
2021-02-01 18:28:08 +0800

    升级Arouter

--- app/src/main/java/com/jianglin/modulecommon/MainActivity.java
+++ app/src/main/java/com/jianglin/modulecommon/MainActivity.java
@@ -4,15 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.jianglin.common.Test;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Test.test();
-
     }
 }
