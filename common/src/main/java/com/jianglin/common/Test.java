96c694fa4748ee0a29f228b5cca8d09a361591e6
jiang <jianglinair@163.com>
2021-02-01 17:01:24 +0800

    升级Arouter

--- common/src/main/java/com/jianglin/common/Test.java
+++ common/src/main/java/com/jianglin/common/Test.java
@@ -2,6 +2,8 @@ package com.jianglin.common;
 
 import android.util.Log;
 
+import com.alibaba.android.arouter.launcher.ARouter;
+
 /**
  * Author: Jianglin
  * Time: 2021/2/1
@@ -14,5 +16,6 @@ public class Test {
 
     public static void test() {
         Log.d(TAG, "[NAIPIQ]test: called");
+        ARouter.debuggable();
     }
 }
